9eb432fad44d694472f8b7e2ea55fd907147b426
Harjeet Singh <singh.harjee98@gmail.com>
2021-01-11 08:00:37 +0530

    error in deployed on heroku

--- COVID_Dashboard/settings.py
+++ COVID_Dashboard/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'zlie)h(2rnb7kxo@9dhzyf4751mqeouo(==2@dgsd%p+varl5g'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['covid-19-django-webapp.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['covid-19-webapp-dashboard.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
